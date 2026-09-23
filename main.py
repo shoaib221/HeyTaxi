900346a20c3bbe2a251b5d8c12d9323736056afa
Shoaib <shoaibtasrif326@gmail.com>
2026-09-23 11:58:42 +0600

    [init]

---
+++ main.py
@@ -0,0 +1,15 @@
+from src.utils.spark_session import get_spark_session
+from src.pipeline.bronze import run_bronze
+from src.pipeline.silver import run_silver
+from src.pipeline.gold import run_gold
+
+if __name__ == "__main__":
+    print("Starting Local Medallion Pipeline...")
+    spark = get_spark_session()
+    
+    run_bronze(spark)
+    run_silver(spark)
+    run_gold(spark)
+    
+    print("Pipeline Execution Finished Successfully!")
+    spark.stop()
